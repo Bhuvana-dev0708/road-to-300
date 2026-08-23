aaffb33955acc3ed9f80e522d52093fd5a77f689
Bhuvanakruthi P. <bhuvanakruthi718@gmail.com>
2026-08-23 23:50:30 +0530

    Learnt python classes

--- basics04.py
+++ basics04.py
@@ -7,7 +7,7 @@ s1 = Student()#object
 
 print(s1.name)
 
-
+#example1
 class Computer:
 
     def config(self):
@@ -16,12 +16,11 @@ class Computer:
 com1 = Computer()#object
 com2 = Computer()
 
-Computer.config(com1)
-com1.config()#both the syntax guves same output but we usually use second one
+com1.config()
 com2.config()
 
 
-#using init(special method)
+#using __init__(special method)
 class Computer:
 
     def __init__(self,cpu,ram):
@@ -29,12 +28,12 @@ class Computer:
         self.ram = ram
 
     def config(self):
-        print("Config is",self.cpu,self.ram) 
+        print(self.cpu,self.ram) 
 
 com1 = Computer('i5',16)#object
 com2 = Computer('Ryzen 3',8 )
 
-
-com1.config()#both the syntax guves same output but we usually use second one
+com1.config()
 com2.config()
+
 
