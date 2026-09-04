8d8a584cbb698d54754a531741b2b79a7fe86892
Bhuvanakruthi P. <bhuvanakruthi718@gmail.com>
2026-09-04 22:38:01 +0530

    Solved 3 leetcode problems

---
+++ Day 2/Group Anagrams.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freq_words = defaultdict(list)
+
+        def count(word):
+            freq={}
+
+            for char in word:
+                freq[char] = freq.get(char,0) + 1
+
+            return tuple(sorted(freq.items()))
+        
+        for word in strs:
+            key = count(word)
+            
+            freq_words[key].append(word)
+        
+        return list(freq_words.values())
